07d3ab448ff600f7007340c12b1af87d0b4922b9
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-03-06 10:26:07 +0700

    Create Google.sql

---
+++ Database/Google.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Mar 2019 pada 04.24
+-- Versi server: 10.1.32-MariaDB
+-- Versi PHP: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `Google`
+--
+
+CREATE TABLE `google` (
+  `i` int(11) NOT NULL,
+  `d` int(11) NOT NULL,
+  `s` int(11) NOT NULL,
+  `szx` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
