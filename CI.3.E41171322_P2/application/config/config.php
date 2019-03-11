b17d898b8ea4c7bec1076776ec791d3a1491b4d6
galangadji <galang7116@gmail.com>
2019-03-11 11:25:38 +0700

    11/03/2019

--- CI.3.E41171322_P2/application/config/config.php
+++ CI.3.E41171322_P2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/galangadji';
+$config['base_url'] = 'http://localhost/CI.3.E41171322_P2/';
 
 /*
 |--------------------------------------------------------------------------
