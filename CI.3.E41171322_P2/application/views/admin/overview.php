362a76ecca822e734b4ac74adb973c30fffcd17c
galangadji <galang7116@gmail.com>
2019-03-11 10:51:10 +0700

    11/03/2019

--- CI.3.E41171322_P2/application/views/admin/overview.php
+++ CI.3.E41171322_P2/application/views/admin/overview.php
@@ -11,6 +11,9 @@
 
   <title>SB Admin - Dashboard</title>
 
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
