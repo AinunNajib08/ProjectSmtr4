7fe920a30e32a891eda91c89ca154aca45bde0c9
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-03-14 10:57:59 +0700

    AD

---
+++ AD.4.E41171178.INDIVIDU/HalloToast2/app/src/main/java/android/example/com/HalloRelative/MainActivity.java
@@ -0,0 +1,38 @@
+package android.example.com.HalloRelative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private int i=10;
+    private int data = 10;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_button_toast, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mCount == data){
+            Log.d("MainActivity", "Count Ke "+data+"");
+            data+=i;
+        }
+        if (mShowCount !=null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+}
