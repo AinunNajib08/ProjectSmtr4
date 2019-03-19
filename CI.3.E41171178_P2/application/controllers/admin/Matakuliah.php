477cc7dbf3499feee099b6bbaa0f618a1c3e7fe1
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-03-19 21:24:21 +0700

    i

---
+++ CI.3.E41171178_P2/application/controllers/admin/Matakuliah.php
@@ -0,0 +1,61 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Matakuliah extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("matakuliah_model");
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data["matakuliah"] = $this->matakuliah_model->getAll();
+        $this->load->view("admin/tambahdata/list", $data);
+    }
+
+    public function add()
+    {
+        $product = $this->matakuliah_model;
+        $validation = $this->form_validation;
+        $validation->set_rules($product->rules());
+
+        if ($validation->run()) {
+            $product->save();
+            $this->session->set_flashdata('success', 'Berhasil disimpan');
+        }
+
+        $this->load->view("admin/tambahdata");
+    }
+
+    public function edit($id = null)
+    {
+        if (!isset($id)) redirect('admin/tambahdata');
+
+        $product = $this->matakuliah_model;
+        $validation = $this->form_validation;
+        $validation->set_rules($product->rules());
+
+        if ($validation->run()) {
+            $product->update();
+            $this->session->set_flashdata('success', 'Berhasil disimpan');
+        }
+
+        $data["matakuliah"] = $product->getById($id);
+        if (!$data["matakuliah"]) show_404();
+
+        $this->load->view("admin/tambahdata/edit_form", $data);
+    }
+
+    public function delete($kode_matkul = null)
+    {
+        if (!isset($kode_matkul)) show_404();
+
+        if ($this->matakuliah_model->delete($kode_matkul)) {
+            redirect(site_url('admin/tambahdata'));
+        }
+    }
+}
--- CI.3.E41171178_P2/application/controllers/tambahdata.php
+++ CI.3.E41171178_P2/application/controllers/tambahdata.php
@@ -1,22 +1,13 @@
 <?php
 
-class tambahdata extends CI_controller{
-    function index(){
+class tambahdata extends CI_controller
+{
+    function index()
+    {
         $this->load->view("admin/nav.php");
-        $this->load->view("admin/sidebar.php");
-        $this->load->view("admin/dashboor.php");
-            $this->load->view("admin/footer.php");
-            $this->load->view("admin/js.php");
+        $this->load->view("admin/sidebar-tambahdata.php");
+        $this->load->view("admin/tambahdata.php");
+        $this->load->view("admin/footer.php");
+        $this->load->view("admin/js.php");
     }
 }
-
-class data extends CI_controller{
-    function index(){
-        $this->load->view("admin/nav.php");
-        $this->load->view("admin/sidebar.php");
-        $this->load->view("admin/dashboor.php");
-            $this->load->view("admin/footer.php");
-            $this->load->view("admin/js.php");
-    }
-}
-?>
