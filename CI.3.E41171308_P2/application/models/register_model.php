d73ac26e4f58c6f676891a1f6f84a07fabb72575
bawik ardiyan <ardiyanramadhan4@gmail.com>
2019-03-20 12:11:37 +0700

    baru

--- CI.3.E41171308_P2/application/models/register_model.php
+++ CI.3.E41171308_P2/application/models/register_model.php
@@ -14,8 +14,75 @@ class register_model extends CI_Model{
 
     public function rules()
     {
-        return
+        return [
+            ['field' => 'username',
+            'label' => 'username',
+            'rules' => 'required'],
 
+            ['field' => 'password',
+            'label' => 'password',
+            'rules' => 'required'],
+             
+            ['field' => 'nama_lengkap',
+            'label' => 'nama lengkap',
+            'rules' => 'required'],
+
+            ['field' => 'tempat_lahir',
+            'label' => 'tempat lahir',
+            'rules' => 'required'],
+
+            ['field' => 'tanggal_lahir',
+            'label' => 'tanggal lahir',
+            'rules' => 'numeric'],
+
+            ['field' => 'alamat',
+            'label' => 'alamat',
+            'rules' => 'required'],
+
+            ['field' => 'email',
+            'laber' => 'email',
+            'rules' => 'required'],
+            
+        ];
+    }
+
+    public function getALL()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where($this->_table, ["id" => $id])->row()
+    }
+
+    public function save()
+    {
+        $_POST = $this->input->$_POST();
+        $this->id = uniqid();
+        $this->username = $_POST["username"];
+        $this->password = $_POST["password"];
+        $this->nama_lengkap = $_POST["nama lengkap"];
+        $this->tempat_lahir = $_POST["tempat lahir"];
+        $this->tanggal_lahir = $_POST["tanggal lahir"];
+        $this->alamat = $_POST["alamat"];
+        $this->email = $_POST["email"];
+        $this->db->insert($this->_table, $this);
+
+    }
+
+    public function update()
+    {
+        $_POST = $this->input->$_POST();
+        $this->id = uniqid();
+        $this->username = $_POST["username"];
+        $this->password = $_POST["password"];
+        $this->nama_lengkap = $_POST["nama lengkap"];
+        $this->tempat_lahir = $_POST["tempat lahir"];
+        $this->tanggal_lahir = $_POST["tanggal lahir"];
+        $this->alamat = $_POST["alamat"];
+        $this->email = $_POST["email"];
+        $this->db->update($this->_table, $this, array('id' => $_POST['id']))
     }
 
 }
