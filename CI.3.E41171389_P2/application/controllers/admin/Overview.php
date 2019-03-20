950cb851d3a840b72db34e0007e7e00b449c76af
waragafila <ajicheater@gmail.com>
2019-03-20 10:39:57 +0700

    Rabu

--- CI.3.E41171389_P2/application/controllers/admin/Overview.php
+++ CI.3.E41171389_P2/application/controllers/admin/Overview.php
@@ -8,7 +8,12 @@ class Overview extends CI_Controller {
 
 	public function index()
 	{
+<<<<<<< HEAD
         // load view siswa/overview.php
         $this->load->view("siswa/overview");
+=======
+        // load view admin/overview.php
+        $this->load->view("admin/tampilanawal");
+>>>>>>> parent of d1ea2f6... Perbaikan Total
 	}
 }
